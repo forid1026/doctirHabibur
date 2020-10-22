85d573e4d70a7ff6df1caddf2485039610275cec
exitdump <sheikhforid1026@gmail.com>
2020-10-22 18:56:21 +0600

    2 footer widget done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faEnvelope, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faEnvelope, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => {
     return (
         <div className="container">
@@ -14,7 +14,7 @@ const Footer = () => {
                             </div>
                             <div className="footer-body">
                                 <div className="footer-gallery">
-                                    <ul>
+                                    <ul className="list-unstyled list-inline">
                                         <li>
                                             <a href="javaScript:void(0)">
                                                 <img src="https://i.ibb.co/dgRYX7t/ex-1-b1abe022.jpg" width="80px" className="rounded float-left" alt="" />
@@ -25,6 +25,7 @@ const Footer = () => {
                                                 <img src="https://i.ibb.co/CPyxz97/ex-2-cc49857d.jpg" className="rounded float-left" width="80px" alt="" />
                                             </a>
                                         </li>
+                                       
                                         <li>
                                             <a href="javaScript:void(0)">
                                                 <img src="https://i.ibb.co/9gCYZCJ/ex-3-77fb1f56.jpg" className="rounded float-left" width="80px" alt="" />
@@ -35,6 +36,7 @@ const Footer = () => {
                                                 <img src="https://i.ibb.co/rc2qhwW/ex-4-54372105.jpg" className="rounded float-left" width="80px" alt="" />
                                             </a>
                                         </li>
+                                      
                                         <li>
                                             <a href="javaScript:void(0)">
                                                 <img src="https://i.ibb.co/PDWRtCD/ex-5-ec810cb2.jpg" className="rounded float-left" width="80px" alt="" />
@@ -58,43 +60,44 @@ const Footer = () => {
                             <div className="footer-box-body">
                                 <div className="text-widget">
                                     <div className="c-des">
-                                        <FontAwesomeIcon icon={faMapMarker}  className="span" />
                                         <br/>
+                                        <FontAwesomeIcon icon={faMapMarker}  className="span" />
                                             <strong className="english-f praava">
                                                 <a className="text-dark" href="http://praavahealth.com">
                                                     PRAAVA HEALTH
                                                 </a>
                                                 <br />
-                                                <p class="p-slog">Family Doctors + Diagnostics</p>
+                                                <p class="p-slog mt-4">Family Doctors + Diagnostics</p>
                                             </strong><br />
                                             <p class="sub-text">বনানী পোস্ট অফিস লেন (স্টার কাবাব রেস্তোরাঁর পাশে)
                                                 <br />
                                                 রোড নং ১৭, ব্লকঃ সি
                                             </p>
                                     </div>
-                                    <p class="dt-sc-contact-info"><span class="fa fa fa-phone"></span></p>
+                                   
                                     <div class="c-des">
-                                        <FontAwesomeIcon icon={faPhone} className="span"/>
+                                        <FontAwesomeIcon icon={faPhone} className="span" />
                                         <strong> সিরিয়ালের জন্য: </strong>
                                         <br/>
                                             <a href="tel:09678701701" className="text-dark">০৯৬৭৮৭০১৭০১</a>
-                                            <a href="tel:01847278043" className="text-dark"></a>  
-                                            <a href=""></a>
+                                            <br/>
+                                            <a href="tel:01847278043" className="text-dark"> </a>  
+                                            <a href=""></a> 
                                      </div>
-                                        <p class="dt-sc-contact-info">
-                                            <span class="fa fa fa-clock"></span>
-                                        </p>
+                                        
                                         <div class="c-des">
-                                            <strong>প্রতিদিন সময়:</strong>
+                                        <FontAwesomeIcon icon={faClock} className="span" />
+                                            <strong> প্রতিদিন সময়:</strong>
                                             <br/>৫:৩০ হতে রাত ৮:০০ টা (বৃহস্পতিবার বন্ধ)
                                         </div>
-                                        <p class="dt-sc-contact-info">
-                                            <FontAwesomeIcon icon={faEnvelope} />
-                                        </p>
+                                        
+                                        
+                                       
                                         <div class="c-des">
+                                        
                                         <FontAwesomeIcon icon={faEnvelope} className="span" />
                                             <a href="mailto:drhabib97@gmail.com" class="english-f" className="text-dark"> drhabib97@gmail.com</a>
-                                        </div>
+                                        </div> 
                                     </div>
                                 </div>
 
